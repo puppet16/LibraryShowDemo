46d9d132f2243a1473f1d28eb81fc2596f155e97
litongtong <2542469086@qq.com>
2019-06-19 19:32:27 +0800

    SwitchIcon示例完成

--- app/src/main/java/com/luck/libraryshowdemo/activity/SwitchIconActivity.java
+++ app/src/main/java/com/luck/libraryshowdemo/activity/SwitchIconActivity.java
@@ -1,9 +1,15 @@
 package com.luck.libraryshowdemo.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.github.zagum.switchicon.SwitchIconView;
 import com.luck.libraryshowdemo.R;
 import com.luck.libraryshowdemo.base.BaseActivity;
+
+import butterknife.BindArray;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class SwitchIconActivity extends BaseActivity {
 
@@ -11,16 +17,12 @@ public class SwitchIconActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switchicon);
-        initView();
-        initData();
     }
 
-    private void initView() {
-
+    @OnClick({R.id.switchIconView1, R.id.switchIconView2, R.id.switchIconView3,
+            R.id.switchIconView4, R.id.switchIconView5, R.id.switchIconView6, R.id.switchIconView7,
+            R.id.switchIconView8, R.id.switchIconView9})
+    public void onClick(SwitchIconView v) {
+        v.switchState();
     }
-
-    private void initData() {
-
-    }
-
 }
